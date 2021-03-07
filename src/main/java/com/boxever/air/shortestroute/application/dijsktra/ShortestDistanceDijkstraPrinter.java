b6481f2c82784365860a0b3aed241c442f019e8b
tolo.dols <tolo.dols@externos.w2m.com>
2021-03-07 23:00:40 +0100

    ShortestRoutePrinter => Printer Implementation

--- src/main/java/com/boxever/air/shortestroute/application/dijsktra/ShortestDistanceDijkstraPrinter.java
+++ src/main/java/com/boxever/air/shortestroute/application/dijsktra/ShortestDistanceDijkstraPrinter.java
@@ -9,6 +9,26 @@ public class ShortestDistanceDijkstraPrinter implements ShortestDistancePrinter<
 
     @Override
     public String print(RouteNode result) {
-        return "";
+        StringBuilder builder = new StringBuilder();
+        for (int i = 1; i <= result.getShortestRoute().size(); i++) {
+            RouteNode departureNode = result.getShortestRoute().get(i-1);
+            RouteNode arrivalNode = i==result.getShortestRoute().size() ? result : result.getShortestRoute().get(i);
+            builder.append(printSingleRoute(departureNode, arrivalNode));
+        }
+        builder.append(printTotalTime(result));
+        return builder.toString();
+    }
+
+    private StringBuilder printSingleRoute(final RouteNode departureNode, final RouteNode arrivalNode) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(departureNode.getAirport()).append(" -- ").append(arrivalNode.getAirport())
+                .append(" (").append(arrivalNode.getTotalDistance()- departureNode.getTotalDistance()).append(")").append("\n");
+        return builder;
+    }
+
+    private StringBuilder printTotalTime(RouteNode arrivalNode) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Time: ").append(arrivalNode.getTotalDistance());
+        return builder;
     }
 }
